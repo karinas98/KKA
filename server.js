5263e8187dbc86cc0d1dd7e87435c0435766e17f
ALI <fsmaccounting@hotmail.com>
2023-03-14 21:51:29 +0000

    tuesday session 2

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ import express from 'express';
 const app = express();
 const PORT = 2000;
 
-app.listen(PORT, () => {
+
+app.use(express.json())
+app.use(logger)
+app.use(router)
+app.use(fallback)
+app.use(errorhandler)
+
+const startServer = async () => { app.listen(PORT, () => {
   console.log(`server is working on ${PORT}`);  
 });    
+}
+startServer();
  
