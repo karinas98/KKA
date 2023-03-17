e7329b8429cdbdf61c5fff0f1dfc80a219a3a4a8
ALI <fsmaccounting@hotmail.com>
2023-03-17 00:04:26 +0000

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,31 @@
-const auth = async(req,res,next) => {
-  
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "../consts.js";
+import User from "../models/user.js";
+
+export const auth = async(req,res,next) => {
+    const rawToken = req.headers.authorization;
+    const token = rawToken.replace("Bearer ", ""); 
+
+    
+     if (!rawToken) {
+         return res.status(403).son({ message: "No token provided" });
+       }
+      try {
+        const decodedToken = jwt.verify(token, JWT_SECRET);
+        const foundUser = await User.findById(decodedToken.id).select(
+          "email userName id role"
+        );
+        if (!foundUser) {
+          return res
+            .status(403)
+            .json({ message: "User with this id does not exist in our database" });
+        }
+        req.currentUser = foundUser;
+        next();
+    }
+        catch (err){
+            next(err)
+        }
 }
+
+export default auth;
--- router.js
+++ router.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import foodController from './controllers/foodController.js';
 import userController from './controllers/userController.js';
+import { auth } from './middleware/auth.js';
+
 
 const router = express.Router();
 router.route('/foods').get(foodController.getAll).post(foodController.create);
 router
   .route('/foods/:id')
   .get(foodController.getById)
-  .patch(foodController.updateById)
-  .delete(foodController.deleteById);
+  .patch(auth,foodController.updateById)
+  .delete(auth,foodController.deleteById);
 
 router.route('/register').post(userController.register);
 router.route('/login').post(userController.login);
