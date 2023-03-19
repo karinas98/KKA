125172c6e2bc01c713d970f857f4c78734955d72
ALI <fsmaccounting@hotmail.com>
2023-03-19 22:09:57 +0000

    19/03/23/

--- FrontEnd/src/pages/Register.jsx
+++ FrontEnd/src/pages/Register.jsx
@@ -1,23 +1,73 @@
-import { useState } from "react";
+import { useState } from 'react';
+import axios from 'axios';
+import { API_URL } from '../consts-data';
+
 const Register = () => {
-    const [FormData, setFromData]=useState({
-        userName: "",
-        email: "",
-        password: "",
-        ConfirmPassword: "",
-    })
-return (
-  <div>
-    <h1>Register</h1>
-    <form>
-      <input placeholder="UserName" name="userName" />
-      <input placeholder="Email" name="email" />
-      <input placeholder="Password*" name="password" />
-      <input placeholder="Confirm Password*" name="confirmPassword" />
-      <button type="submit">Register</button>
-    </form>
-  </div>
-);
-}
+  // const initialFormData = {
+  //   userName: "",
+  //   email: "",
+  //   password: "",
+  //   confirmPassword: "",
+  // };
+  const [error, setError] = useState('');
+  const [message, setMessage] = useState('');
+  const [formData, setFormData] = useState({
+    userName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const onChange = (e) => {
+    console.log(e.target.value);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${API_URL}/register`, formData);
+      console.log(res);
+      setMessage(response.code);
+      setFormData(formData);
+    } catch (err) {
+      console.log(err);
+      // setError(response.data.message);
+    }
+  };
+  return (
+    <div>
+      {message && <h4>{message}</h4>}
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder="UserName"
+          name="userName"
+          onChange={onChange}
+          value={formData.userName}
+        />
+        <input
+          placeholder="Email"
+          name="email"
+          value={formData.email}
+          onChange={onChange}
+        />
+        <input
+          onChange={onChange}
+          placeholder="Password*"
+          name="password"
+          value={formData.password}
+        />
+        <input
+          placeholder="Confirm Password*"
+          name="confirmPassword"
+          onChange={onChange}
+          value={formData.confirmPassword}
+        />
+        <button type="submit">Register</button>
+      </form>
+
+      <h1>Register</h1>
+    </div>
+  );
+};
 
 export default Register;
