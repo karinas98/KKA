e85a10cd3b876278cad23c916e1fd39d994e57c3
ALI <fsmaccounting@hotmail.com>
2023-03-22 10:36:16 +0000

    project3

--- BackEnd/consts.js
+++ BackEnd/consts.js
@@ -1,10 +1,9 @@
-import dotnenv from "dotenv";
+import dotenv from "dotenv";
 
-dotnenv.config();
+dotenv.config();
 
 export const DB_CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING ||
-  'mongodb+srv://root:qhFbdUl81l8nQgri@cluster0.mx6bwrl.mongodb.net/foodsdb?retryWrites=true';
+  process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017';
 
-export const JWT_SECRET = process.env.JWT_SECRET || 'VdakT0mCeF7gnj';
+export const JWT_SECRET = process.env.JWT_SECRET || 'whatever';
 console.log({DB_CONNECTION_STRING, JWT_SECRET})
