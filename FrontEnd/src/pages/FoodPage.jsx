bbbbe6a324bb0f6300f62e9b2ac2f580c2f38819
Karina Savoie <karina98savoie@gmail.com>
2023-03-23 19:17:42 +0100

    Styling Update

--- FrontEnd/src/pages/FoodPage.jsx
+++ FrontEnd/src/pages/FoodPage.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { API_URL } from '../consts-data';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { API_URL } from "../consts-data";
 
 const FoodPage = () => {
   // const onChangeHandler = (e) => {
@@ -14,7 +14,7 @@ const FoodPage = () => {
       try {
         const res = await axios.get(`${API_URL}/foods/${foodId}`);
         setFood(res.data.data);
-        console.log('hello');
+        console.log("hello");
       } catch (err) {
         console.log(err);
       }
@@ -25,18 +25,23 @@ const FoodPage = () => {
   return (
     <div>
       <ul>
-        <div>
+        <div className="food-page">
+          <span className="food-header">
+            <span className="header-left">
+              <li>
+                <h1 className="inv-title">{food.name}</h1>
+              </li>
+              <li>
+                <h2 className="inv-origin">{food.origin}</h2>
+              </li>
+            </span>
+            <li className="header-right">
+              <img width="100%" src={food.foodUrl} />
+            </li>
+          </span>
+
           <li>
-            <h1>{food.name}</h1>
-          </li>
-          <li>
-            <h2>{food.origin}</h2>
-          </li>
-          <li>
-            <img src={food.foodUrl} />
-          </li>
-          <li>
-            <p>{food.description}</p>
+            <p className="inv-desc">{food.description}</p>
           </li>
         </div>
       </ul>
