20838b35bc6138d705c436030ddf5f304ca4b91e
KONSTANTINOS <kostasfergadis74@gmail.com>
2023-03-23 20:25:00 +0200

    listkinda

--- FrontEnd/src/pages/Explore.jsx
+++ FrontEnd/src/pages/Explore.jsx
@@ -1,13 +1,11 @@
-
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { API_URL } from '../consts-data';
-import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { API_URL } from "../consts-data";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 //import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
-
+import Dropdown from "react-bootstrap/Dropdown";
 
 const Explore = () => {
   const [foods, setFoods] = useState([]);
@@ -26,7 +24,7 @@ const Explore = () => {
     foodsArray.sort((a, z) => {
       return parseInt(z.name) - parseInt(a.name);
     });
-    if (inputValue.trim() === '') {
+    if (inputValue.trim() === "") {
       setSearchFilter([]);
     } else {
       setFoods(foodsArray);
@@ -36,18 +34,15 @@ const Explore = () => {
       setSearchFilter(filteredFoods);
       console.log(searchFilter);
     }
-
   };
 
-  // const addToMyList = async (foodId) => {
-  //   try {
-  //     await axios.post(`${API_URL}/myList`, { foodId });
-  //     // Display a success message or update the list state
-  //   } catch (error) {
-  //     console.error(error);
-  //     // Display an error message
-  //   }
-  // };
+  const addToMyList = async (foodId) => {
+    try {
+      await axios.post(`${API_URL}/myList`, { foodId });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   // const onClick = async () => {
   //   const res = await axios.get(`${API_URL}/users`);
@@ -75,7 +70,6 @@ const Explore = () => {
 
   return (
     <div>
-
       <form className="search-form">
         <input
           className="input-search"
@@ -85,7 +79,7 @@ const Explore = () => {
           value={searchInput}
         />
         {searchFilter.map((element) => (
-          <Dropdown.Item key={element._id} href={`#/foods/${element._id}`}>
+          <Dropdown.Item key={element._id} to={`/foods/${element._id}`}>
             <Link to={`/foods/${element._id}`}>
               <span className="dropdown-search">
                 <img className="flag-card" src={element.flagUrl} />
@@ -96,9 +90,6 @@ const Explore = () => {
         ))}
       </form>
 
-   
-
-
       {isLoading ? (
         <p>Loading</p>
       ) : (
@@ -107,9 +98,6 @@ const Explore = () => {
             <ul key={ind}>
               <Card style={{ width: "30rem" }}>
                 <Link to={`/foods/${element._id}`}>
-
-
-
                   <div>
                     <li>
                       <Card.Img
@@ -123,7 +111,7 @@ const Explore = () => {
                         <li>
                           <Card.Title>{element.name}</Card.Title>
                         </li>
-                        <button>
+                        <button onClick={() => addToMyList(element._id)}>
                           <img
                             className="list-icon"
                             src="https://res.cloudinary.com/de9zdtobn/image/upload/v1679488194/icons8-add-to-list-64_kuuyn6.png"
--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,21 +1,19 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { API_URL } from '../consts-data';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import axios from "axios";
+import { API_URL } from "../consts-data";
+import { useNavigate } from "react-router-dom";
 
-const login = () => {
+const Login = () => {
   // const initialFormData = {
   //   userName: "",
   //   email: "",
   //   password: "",
   //   confirmPassword: "",
   // };
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [formData, setFormData] = useState({
-
-    email: '',
-    password: '',
-
+    email: "",
+    password: "",
   });
 
   const onChange = (e) => {
@@ -23,29 +21,30 @@ const login = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
-      const  {data}  = await axios.post(`${API_URL}/login`, formData);
-      console.log(data.token);
-      localStorage.setItem("token", data.token)
-      
-      //this code below set default headers
-    //   axios.defaults.headers.common["Authorization"]=`Bearer ${token}`
+      const response = await axios.post(`${API_URL}/login`, formData);
+      console.log("Response:", response);
+      const { data } = response;
+      console.log("Token:", data.token);
+      const token = data.token;
+      localStorage.setItem("token", token);
+      //axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
       setFormData(formData);
-      navigate("/")
+      navigate("/");
     } catch (err) {
+      console.error("Error:", err);
       setError(err.response.data.message);
     }
   };
   return (
     <div>
-          <h1>Login</h1>
+      <h1>Login</h1>
       {error && <h4>{error}</h4>}
       <form onSubmit={onSubmit}>
-    
         <input
           placeholder="Email"
           name="email"
@@ -60,10 +59,8 @@ const login = () => {
         />
         <button type="submit">login</button>
       </form>
-
-    
     </div>
   );
 };
 
-export default login;
+export default Login;
