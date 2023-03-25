70c66b4693de1fc804ac1c2b1fd0c7a5e93e438a
KONSTANTINOS <kostasfergadis74@gmail.com>
2023-03-25 15:09:49 +0200

    deployment

--- FrontEnd/src/consts-data.jsx
+++ FrontEnd/src/consts-data.jsx
@@ -1,1 +1,4 @@
-export const API_URL = 'http://localhost:2001';
+const DEV_API_URL = "http://localhost:2001";
+const PROD_API_URL = "https://kka5.onrender.com";
+export const API_URL =
+  process.env.NODE_ENV === "production" ? PROD_API_URL : DEV_API_URL;
